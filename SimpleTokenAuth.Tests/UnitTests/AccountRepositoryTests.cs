2e8e8ec8c9087a0b506598abef44e075eb400980
alexandrebl <abrandao@stone.com.br>
2017-04-07 19:38:32 -0300

    ajustes

--- SimpleTokenAuth.Tests/UnitTests/AccountRepositoryTests.cs
+++ SimpleTokenAuth.Tests/UnitTests/AccountRepositoryTests.cs
@@ -275,5 +275,115 @@ namespace SimpleTokenAuth.Tests.UnitTests {
             //Verifiy if is true
             Assert.True(authAccounts.Count() == accountList.AuthAccounts.Count);
         }
+
+        /// <summary>
+        /// Insertion test
+        /// </summary>
+        [Fact]
+        public void InsertAccount() {
+            //Login data
+            const string login = "abc";
+            //Password data
+            const string password = "123";
+
+            //Totken data
+            var tokenData = new TokenData() {
+                //Expiuration totken date
+                ExpirationDate = DateTime.UtcNow.AddMinutes(30),
+                //Token data
+                Token = Guid.NewGuid().ToString()
+            };
+
+            //Account list
+            var accountList = new AccountList() {
+                //Auth accoint list
+                AuthAccounts ={
+                    {"def", new AuthAccount("def", "456") { TokenData = new TokenData(){ExpirationDate = DateTime.UtcNow.AddMinutes(30), Token = Guid.NewGuid().ToString()}}},
+                    {"fgh", new AuthAccount("fgh", "789") { TokenData = new TokenData(){ExpirationDate = DateTime.UtcNow.AddMinutes(30), Token = Guid.NewGuid().ToString()}}},
+                    {"ijl", new AuthAccount("ijl", "000") { TokenData = new TokenData(){ExpirationDate = DateTime.UtcNow.AddMinutes(30), Token = Guid.NewGuid().ToString()}}},
+                }
+            };
+
+            //Account repository
+            var accountRepository = new AccountRepository(accountList);
+
+            //Total of account before insertion
+            var accountsBeforeInsertion = accountRepository.GetAllAccounts().Count();
+            
+            //Insert account
+            var result = accountRepository.Insert(login, password);
+
+            //Total of account after insertion
+            var accountsAfterInsertion = accountRepository.GetAllAccounts().Count();
+
+            //Verify if is true
+            Assert.True(result);
+            //Verify is is null
+            Assert.NotNull(accountsBeforeInsertion);
+            //Verify is is null
+            Assert.NotNull(accountsAfterInsertion);
+            //Verify if are equals
+            Assert.Equal(accountsAfterInsertion, accountsBeforeInsertion + 1);
+        }
+
+        /// <summary>
+        /// Insertion test
+        /// </summary>
+        [Fact]
+        public void DeleteAccount() {
+            //Login data
+            const string login = "abc";
+            //Password data
+            const string password = "123";
+
+            //Totken data
+            var tokenData = new TokenData() {
+                //Expiuration totken date
+                ExpirationDate = DateTime.UtcNow.AddMinutes(30),
+                //Token data
+                Token = Guid.NewGuid().ToString()
+            };
+
+            //Account list
+            var accountList = new AccountList() {
+                //Auth accoint list
+                AuthAccounts ={
+                    {"def", new AuthAccount("def", "456") { TokenData = new TokenData(){ExpirationDate = DateTime.UtcNow.AddMinutes(30), Token = Guid.NewGuid().ToString()}}},
+                    {"fgh", new AuthAccount("fgh", "789") { TokenData = new TokenData(){ExpirationDate = DateTime.UtcNow.AddMinutes(30), Token = Guid.NewGuid().ToString()}}},
+                    {"ijl", new AuthAccount("ijl", "000") { TokenData = new TokenData(){ExpirationDate = DateTime.UtcNow.AddMinutes(30), Token = Guid.NewGuid().ToString()}}},
+                }
+            };
+
+            //Account repository
+            var accountRepository = new AccountRepository(accountList);
+
+            //Total of account before insertion
+            var accountsBeforeInsertion = accountRepository.GetAllAccounts().Count();
+
+            //Insert account
+            var result1 = accountRepository.Insert(login, password);
+
+            //Total of account after insertion
+            var accountsAfterInsertion = accountRepository.GetAllAccounts().Count();
+
+            //Insert account
+            var result2 = accountRepository.Delete(login);
+
+            //Total of account after insertion
+            var accountsAfterDeletion = accountRepository.GetAllAccounts().Count();
+
+            //Verify if is true
+            Assert.True(result1);
+            //Verify if is true
+            Assert.True(result2);
+            //Verify is is null
+            Assert.NotNull(accountsBeforeInsertion);
+            //Verify is is null
+            Assert.NotNull(accountsAfterInsertion);
+            //Verify if are equals
+            Assert.Equal(accountsAfterInsertion, accountsBeforeInsertion + 1);
+            //Verify if are equals
+            Assert.Equal(accountsAfterInsertion, accountsAfterDeletion);
+        }
     }
 }
